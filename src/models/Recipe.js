b5371f41f0109ac2656b92e8021e913201838669
aichetou1408 <aichetouahmedgaye@gmail.com>
2024-09-16 13:01:19 +0100

    Added models

---
+++ src/models/Recipe.js
@@ -0,0 +1,48 @@
+import { pool } from "../config/db.js";
+class Recipe {
+  static async checkRecipe(titre) {
+    const connection = await pool.getConnection();
+    const [result] = await connection.execute(
+      "select * from recettes where titre = ?",
+      [titre]
+    );
+    return result.length;
+  }
+
+  static async getRecipeById(id) {
+    const connection = await pool.getConnection();
+    const [result] = await connection.execute(
+      "select * from recettes where id = ?",
+      [id]
+    );
+    return result.length;
+  }
+
+  static async getRecipes() {
+    const connection = await pool.getConnection();
+    const [result] = await connection.execute("select * from recettes");
+    return result;
+  }
+
+  static async createRecipe(titre, ingredients, type) {
+    const connection = await pool.getConnection();
+    await connection.execute(
+      "INSERT INTO recettes(titre, ingredients, type) VALUES (?, ?, ?)",
+      [titre, ingredients, type]
+    );
+  }
+
+  static async delRecipe(id) {
+    const connection = await pool.getConnection();
+    await connection.execute("delete from recettes where id = ?", [id]);
+  }
+  static async editRecipe(id, titre, ingredients, type) {
+    const connection = await pool.getConnection();
+    await connection.execute(
+      "update recettes set titre = ?, ingredients = ?, type = ? where id = ?",
+      [titre, ingredients, type, id]
+    );
+  }
+}
+
+export { Recipe };

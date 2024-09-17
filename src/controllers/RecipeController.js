5189e2573b5780dc5bb9073ac169c5ba003a22af
aichetou1408 <aichetouahmedgaye@gmail.com>
2024-09-17 10:21:04 +0100

    Express validator implementation

---
+++ src/controllers/RecipeController.js
@@ -0,0 +1,63 @@
+import { Recipe } from "../models/Recipe.js";
+
+class RecipeController {
+  static async getByID(req, res, next) {
+    try {
+      const id = req.params.id;
+      const result = await Recipe.getRecipeById(id);
+      res.json(result);
+    } catch (error) {
+      console.log(error.message);
+    }
+    next();
+  }
+  static async getAllRecipes(_req, res, next) {
+    try {
+      const result = await Recipe.getRecipes();
+      res.json(result);
+    } catch (error) {
+      console.log(error.message);
+    }
+    next();
+  }
+
+  static async createRecipe(req, res, next) {
+    try {
+      const titre = req.body.titre;
+      const ingredients = req.body.ingredients;
+      const type = req.body.type;
+      await Recipe.createRecipe(titre, ingredients, type);
+      res.json("Added successfully");
+    } catch (error) {
+      console.log(error.message);
+    }
+    next();
+  }
+
+  static async deleteRecipe(req, res, next) {
+    try {
+      const id = req.params.id;
+      await Recipe.delRecipe(id);
+      res.json("Deleted successfully");
+    } catch (error) {
+      console.log(error.message);
+    }
+    next();
+  }
+
+  static async updateRecipe(req, res, next) {
+    try {
+      const id = req.params.id;
+      const titre = req.body.titre;
+      const ingredients = req.body.ingredients;
+      const type = req.body.type;
+      await Recipe.editRecipe(id, titre, ingredients, type);
+      res.json("Updted successfully");
+    } catch (error) {
+      console.log(error.message);
+    }
+    next();
+  }
+}
+
+export { RecipeController };

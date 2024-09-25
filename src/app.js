3b93e337d6ad9bbed29d6adbbdb142d4f440d6e9
Mohamed Bankolé <medbankole97@gmail.com>
2024-09-25 12:13:33 +0000

    Correction of unit tests

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
 import express from 'express';
 import router from './routes/index.js';
 import bodyParser from 'body-parser';
-import pool from './config/db.js';
+// import pool from './config/db.js';
 import { config } from 'dotenv';
-config()
-
+config();
 
 const app = express();
 app.use(bodyParser.json());
 app.use(router);
 
-const PORT = process.env.NODE_DOCKER_PORT || 3021
+const PORT = process.env.NODE_DOCKER_PORT || 3021;
 app.listen(PORT, () => {
   console.log(`Successfully connected on ${PORT} `);
 });

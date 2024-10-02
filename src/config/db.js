a72d66156dcf15f551c0d0482548b4bd8a97bed0
Mohamed Bankolé <medbankole97@gmail.com>
2024-10-02 11:59:34 +0000

    Retire db.js of gitignore

---
+++ src/config/db.js
@@ -0,0 +1,20 @@
+import mysql from 'mysql2/promise';
+import { config } from 'dotenv';
+
+config();
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 100,
+  connectTimeout: 100000,
+});
+
+pool.getConnection().then(() => {
+  console.log('CONNECTED');
+});
+
+// export { pool };
+export default pool;
